c95765076bc6b86e6f9c6344ec791db9d425ac64
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-11-21 15:08:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# oCURL by DNA ("Output CURL")
+Hello,
+this is a really simple project which takes in a url and automatically saves it to a file
+
+## How to use
+- Usage: `ocurl <url>`
+
+## Compiling
+- Install [Deno](https://deno.land/)
+- Run `deno compile -o ocurl.exe -A main.ts` in the project directory
---
+++ main.ts
@@ -0,0 +1,20 @@
+import {crayon} from "https://deno.land/x/crayon@3.3.3/mod.ts";
+
+const downloadUrl = Deno.args[0] || "";
+
+if (!downloadUrl) {
+	console.error("No URL provided");
+	Deno.exit(1);
+}
+
+const filename = downloadUrl.split("/").at(-1)?.split("?")[0] || "download";
+
+// Download the (internet) file to a local file.
+try {
+	Deno.writeFileSync(filename, new Uint8Array(await (await fetch(downloadUrl)).arrayBuffer()));
+} catch {
+	console.error("Failed to download file");
+	Deno.exit(1);
+}
+
+console.log(`    ${crayon.green(downloadUrl)}\n -> ${crayon.green((Deno.cwd() + "/" + filename).replaceAll("\\", "/"))}`);
