d2c991133375921136875b31f589a008ab0f3fbe
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-12-27 16:53:57 +0100

    Reloaded because of changed import

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import {crayon} from "https://deno.land/x/crayon@3.3.3/mod.ts";
-import progressFetch from "https://dnascanner.de/functions/deno/fetchprogress.ts";
+import fetchProgress from "https://dnascanner.de/functions/deno/fetchprogress.ts";
 
 const downloadUrl = Deno.args[0] || "";
 
@@ -12,7 +12,7 @@ const decodedUrl = decodeURIComponent(downloadUrl);
 const filename = decodedUrl.split("/").at(-1)?.split("?")[0] || "download";
 
 try {
-	await progressFetch(decodedUrl, filename);
+	await fetchProgress(decodedUrl, filename);
 	console.log(`\x1b[1F\x1b[1MDownloaded ${crayon.green(filename)} to ${crayon.green(filename)}`);
 	Deno.exit(0);
 } catch {
